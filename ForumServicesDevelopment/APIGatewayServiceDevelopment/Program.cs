56b9d6d96ea0a48c30d9f6fa2c73732aa5208ce4
vafanasiev <vladumer1994@gmail.com>
2023-07-07 01:23:02 +0300

    Added 'APIGatewayServiceDevelopment' empty project to the solution

---
+++ ForumServicesDevelopment/APIGatewayServiceDevelopment/Program.cs
@@ -0,0 +1,18 @@
+using APIGatewayServiceDevelopment;
+
+public static class Program
+{
+	public static void Main(string[] args)
+	{
+		CreateWebHostBuilder(args).Build().Run();
+	}
+
+	public static IHostBuilder CreateWebHostBuilder(string[] args)
+	{
+		return Host.CreateDefaultBuilder(args)
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+				webBuilder.UseStartup<Startup>();
+			});
+	}
+}
